04eefa7940bb5f3cbdb9bf421591ff9d111b8e36
App Academy Student <student@appacademy.io>
2018-02-23 16:32:39 -0800

    Create int_set and hashing function

--- lru_cache/lib/p01_int_set.rb
+++ lru_cache/lib/p01_int_set.rb
@@ -1,19 +1,28 @@
 class MaxIntSet
+  attr_reader :store
   def initialize(max)
+    @store = Array.new(max)
   end
 
   def insert(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num] = false
   end
 
   def include?(num)
+    raise "Out of bounds" unless is_valid?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num > 0 && num < @store.length
   end
 
   def validate!(num)
@@ -27,15 +36,25 @@ class IntSet
   end
 
   def insert(num)
+    bucket =  get_bucket(num)
+    bucket << num unless bucket.include?(num)
   end
 
   def remove(num)
+    bucket = get_bucket(num)
+    bucket.delete(num)
   end
 
   def include?(num)
+    bucket = get_bucket(num)
+    bucket.include?(num)
   end
 
   private
+
+  def get_bucket(num)
+    @store[num % num_buckets]
+  end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
@@ -55,18 +74,31 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if count >= num_buckets
+    bucket = self[num]
+    unless bucket.include?(num)
+      bucket << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    bucket = self[num]
+    if bucket.include?(num)
+      bucket.delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    bucket = self[num]
+    bucket.include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +106,13 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self.insert(el)
+      end
+    end
   end
 end
--- lru_cache/lib/p02_hashing.rb
+++ lru_cache/lib/p02_hashing.rb
@@ -4,18 +4,25 @@ end
 
 class Array
   def hash
+    result = 0
+    self.each_with_index do |el, idx|
+      result += el.hash * (idx + 1)
+    end
+    result
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
+
 end
 
 class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.sort.hash
   end
 end
